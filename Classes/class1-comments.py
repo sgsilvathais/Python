6da217e40774d694085e83d90061d18343ec948f
Thais Siqueira <sgsilva.thais@gmail.com>
2023-05-24 16:54:42 -0300

    Modified class1.py and rename it to class1-comments.py

---
+++ Classes/class1-comments.py
@@ -0,0 +1,12 @@
+# Permite escrever um comentário. O interpretador pula linhas com cerquilha.
+
+"""
+    Docstrings não são comentários mas muitas vezes são usados como tal. 
+    O interpretador do python não pula as docstrings, ele as lê.
+"""
+
+'''
+As docstrings podem ser escritas tanto com as duplas quanto com aspas simples. 
+'''
+
+
